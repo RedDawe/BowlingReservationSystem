771d43e103b46416e556ea773a9714376475738a
David Dvořák <xdvora10@fi.muni.cz>
2022-03-16 12:27:56 +0100

    Initial Commit

---
+++ src/main/java/cz/reddawe/bowlingreservationsystem/BowlingReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package cz.reddawe.bowlingreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BowlingReservationSystemApplication.class, args);
+    }
+
+}
