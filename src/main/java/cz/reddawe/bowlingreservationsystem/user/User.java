a6eecfa981c80fcd1ee978bc8c2b9be4078d9a3e
David Dvořák <xdvora10@fi.muni.cz>
2022-03-26 12:36:33 +0100

    Implement UserDetailsService and UserDetails

---
+++ src/main/java/cz/reddawe/bowlingreservationsystem/user/User.java
@@ -0,0 +1,80 @@
+package cz.reddawe.bowlingreservationsystem.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity(name = "User")
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    @Column(
+            name = "id",
+            columnDefinition = "bigint",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "username",
+            columnDefinition = "varchar(255)",
+            nullable = false,
+            unique = true
+    )
+    private String username;
+
+    @Column(
+            name = "password",
+            columnDefinition = "char(60)",
+            nullable = false
+    )
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null; // TODO: grant authorities
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
