700505a07685e03eae8f9988c1072f1ce07decb3
David Dvořák <xdvora10@fi.muni.cz>
2022-03-27 17:42:11 +0200

    Complete bowlinglane

--- src/main/java/cz/reddawe/bowlingreservationsystem/bowlinglane/BowlingLaneController.java
+++ src/main/java/cz/reddawe/bowlingreservationsystem/bowlinglane/BowlingLaneController.java
@@ -1,10 +1,11 @@
 package cz.reddawe.bowlingreservationsystem.bowlinglane;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,7 +20,19 @@ public class BowlingLaneController {
     }
 
     @GetMapping(path = "get")
-    public List<Integer> getBowlingLaneNumbers() {
-        return bowlingLaneService.getBowlingLaneNumbers();
+    public List<BowlingLane> getBowlingLanes() {
+        return bowlingLaneService.getBowlingLanes();
+    }
+
+    @PostMapping(path = "add")
+    public ResponseEntity<BowlingLane> addBowlingLane(@RequestBody BowlingLane bowlingLane) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/v1/bowling-lane/add")
+                .toUriString());
+        return ResponseEntity.created(uri).body(bowlingLaneService.addBowlingLane(bowlingLane));
+    }
+
+    @DeleteMapping(path = "remove")
+    public void removeBowlingLane(@RequestBody BowlingLane bowlingLane) {
+        bowlingLaneService.removeBowlingLane(bowlingLane);
     }
 }
