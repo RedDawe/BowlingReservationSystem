ab36eceb90210562965aa2c9fa88bfc4aea63ce8
David Dvořák <xdvora10@fi.muni.cz>
2022-04-07 11:15:36 +0200

    Add httpBasic

--- src/main/java/cz/reddawe/bowlingreservationsystem/security/SecurityConfig.java
+++ src/main/java/cz/reddawe/bowlingreservationsystem/security/SecurityConfig.java
@@ -14,6 +14,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic();
     }
 }
