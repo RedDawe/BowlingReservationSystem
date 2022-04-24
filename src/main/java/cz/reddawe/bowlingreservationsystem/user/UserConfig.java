4f6bfdd278658b7966f9656b2accaae78f920474
David Dvorak <xdvora10@fi.muni.cz>
2022-04-24 11:29:06 +0200

    Add developmnet user configuration

---
+++ src/main/java/cz/reddawe/bowlingreservationsystem/user/UserConfig.java
@@ -0,0 +1,66 @@
+package cz.reddawe.bowlingreservationsystem.user;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class UserConfig {
+
+    private final AuthorityRepository authorityRepository;
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+
+    public UserConfig(AuthorityRepository authorityRepository, RoleRepository roleRepository, UserRepository userRepository) {
+        this.authorityRepository = authorityRepository;
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+    }
+
+    public void configureAuthorities(List<Authority> userAuthorities, List<Authority> managerAuthorities) {
+        Authority reservationManage = new Authority("RESERVATION:MANAGE");
+        Authority bowlingLaneManage = new Authority("BOWLING_LANE:MANAGE");
+
+        userAuthorities.add(
+                reservationManage
+        );
+        managerAuthorities.addAll(List.of(
+                reservationManage,
+                bowlingLaneManage
+        ));
+
+        authorityRepository.saveAll(List.of(
+                reservationManage, bowlingLaneManage
+        ));
+    }
+
+    public Role configureRoles(List<Authority> userAuthorities, List<Authority> managerAuthorities) {
+        Role user = new Role("USER", userAuthorities);
+        Role manager = new Role("MANAGER", managerAuthorities);
+
+        roleRepository.saveAll(List.of(
+                user,
+                manager
+        ));
+
+        return manager;
+    }
+
+    public void configureManager(Role managerRole) {
+        userRepository.save(
+                new User("manager", "poet-homecoming-group-try", managerRole)
+        );
+    }
+
+    @Bean
+    public void doConfiguration() {
+        List<Authority> userAuthorities = new ArrayList<>();
+        List<Authority> managerAuthorities = new ArrayList<>();
+
+        configureAuthorities(userAuthorities, managerAuthorities);
+        Role manager = configureRoles(userAuthorities, managerAuthorities);
+        configureManager(manager);
+    }
+}
