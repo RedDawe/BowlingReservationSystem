742bea4d4b897b789dd5e5543cd36f258b795dfd
David Dvorak <xdvora10@fi.muni.cz>
2022-04-24 12:41:46 +0200

    Fix //TODO:

--- src/main/java/cz/reddawe/bowlingreservationsystem/user/User.java
+++ src/main/java/cz/reddawe/bowlingreservationsystem/user/User.java
@@ -58,7 +58,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null; // TODO: grant authorities
+        return null; //TODO: grant authorities
     }
 
     @Override
