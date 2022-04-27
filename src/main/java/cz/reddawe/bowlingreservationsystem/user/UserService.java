191feca7e52e8645712098e213d88deba3ef07d6
David Dvorak <xdvora10@fi.muni.cz>
2022-04-27 11:15:40 +0200

    Check for null authentication

--- src/main/java/cz/reddawe/bowlingreservationsystem/user/UserService.java
+++ src/main/java/cz/reddawe/bowlingreservationsystem/user/UserService.java
@@ -5,6 +5,7 @@ import cz.reddawe.bowlingreservationsystem.authorization.RoleRepository;
 import cz.reddawe.bowlingreservationsystem.user.iorecords.RoleName;
 import cz.reddawe.bowlingreservationsystem.user.iorecords.UserInput;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,7 +40,10 @@ public class UserService implements UserDetailsService {
     }
 
     public static Optional<User> getCurrentUser() {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) return Optional.empty();
+
+        Object principal = authentication.getPrincipal();
         if (principal == null) throw new RuntimeException("Authentication returned null from getPrincipal");
         if (principal.equals("anonymousUser")) return Optional.empty();
         if (principal.getClass() != User.class) throw new RuntimeException("""
