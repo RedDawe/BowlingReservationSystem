6242d635c0b2d99ee8a7001fc5df60a8c75da566
David Dvorak <xdvora10@fi.muni.cz>
2022-04-30 09:54:44 +0200

    Rename

--- src/main/java/cz/reddawe/bowlingreservationsystem/exceptions/badrequest/ReservationValidationException.java
+++
@@ -1,8 +0,0 @@
-package cz.reddawe.bowlingreservationsystem.exceptions.badrequest;
-
-public class ReservationValidationException extends BadRequestException {
-
-    public ReservationValidationException(String reason) {
-        super("Reservation is invalid", reason);
-    }
-}
