c9b587270e8b7cf96cdf3c7df54b29b0bf383480
David Dvorak <xdvora10@fi.muni.cz>
2022-05-04 09:54:18 +0200

    Add swagger annotations

--- src/main/java/cz/reddawe/bowlingreservationsystem/bowlinglane/BowlingLaneController.java
+++ src/main/java/cz/reddawe/bowlingreservationsystem/bowlinglane/BowlingLaneController.java
@@ -1,10 +1,19 @@
 package cz.reddawe.bowlingreservationsystem.bowlinglane;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
  * Controller for BowlingLane entity.
@@ -22,16 +31,46 @@ public class BowlingLaneController {
         this.bowlingLaneService = bowlingLaneService;
     }
 
+    @Operation(
+            summary = "Returns all bowling lanes ordered by number",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = BowlingLane.class))
+                    ))
+            }
+    )
     @GetMapping(path = "get")
     public List<BowlingLane> getBowlingLanesOrdered() {
         return bowlingLaneService.getBowlingLanesOrdered();
     }
 
+    @Operation(
+            summary = "Creates a new bowling lane",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = BowlingLane.class)
+                    )),
+                    @ApiResponse(responseCode = "400"),
+                    @ApiResponse(responseCode = "403")
+            },
+            security = @SecurityRequirement(name = "BOWLING_LANE:CREATE")
+    )
     @PostMapping(path = "create")
     public ResponseEntity<BowlingLane> createBowlingLane(@RequestBody BowlingLane bowlingLane) {
         return ResponseEntity.ok().body(bowlingLaneService.createBowlingLane(bowlingLane));
     }
 
+    @Operation(
+            summary = "Deletes a bowling lane",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = String.class))
+                    )),
+                    @ApiResponse(responseCode = "400"),
+                    @ApiResponse(responseCode = "403")
+            },
+            security = @SecurityRequirement(name = "BOWLING_LANE:DELETE")
+    )
     @DeleteMapping(path = "delete/{number}")
     public List<String> deleteBowlingLane(@PathVariable("number") int number) {
         return bowlingLaneService.deleteBowlingLane(number);
--- src/main/java/cz/reddawe/bowlingreservationsystem/user/UserController.java
+++ src/main/java/cz/reddawe/bowlingreservationsystem/user/UserController.java
@@ -2,9 +2,17 @@ package cz.reddawe.bowlingreservationsystem.user;
 
 import cz.reddawe.bowlingreservationsystem.user.iorecords.RoleName;
 import cz.reddawe.bowlingreservationsystem.user.iorecords.UserInput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
  * Controller for User entity.
@@ -22,12 +30,38 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Registers a new user",
+            description = """
+                    Username requires:                   
+                     1. 3 to 255 characters
+                     2. Only consists of upper and lowercase letters, digits and .@
+                    
+                    Password requires:
+                     1. 8 to 50 characters
+                     2. Only consists of upper and lowercase letters, digits and @$!%*#?&
+                     3. At least one digit
+                     4. At least one letter (can be either upper or lowercase)
+                    """,
+            responses = {
+                    @ApiResponse(responseCode = "204"),
+                    @ApiResponse(responseCode = "400")
+            }
+    )
     @PostMapping("register")
     public ResponseEntity<Void> registerUser(@RequestBody UserInput userInput) {
         userService.registerUser(userInput);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Returns role of the currently logged-in user",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = RoleName.class)
+                    ))
+            }
+    )
     @GetMapping("role")
     public RoleName getMyRoleName() {
         return userService.getMyRoleName();
